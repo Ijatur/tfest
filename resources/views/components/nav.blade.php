c0a8a604c40570f6e094f0e6d905d18240dea2b0
ijatur <turamadhanizza@gmail.com>
2025-01-23 00:17:15 +0700

    first-commit

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,17 @@
+<nav class="fixed top-0 items-center justify-between w-full px-10 bg-white shadow-md flex p-6 z-50 bg-yellow">
+    <a href="/"><img class="h-16" src="https://pestapora.com/_nuxt/logo-pestapora-2025.MuvH88kW.png" alt="" srcset=""></a>
+    <ul class="flex gap-8 w-full justify-end hidden md:flex">
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">Ticket</a></li>
+        <li><a href="/lineup">Line up</a></li>
+        <li><a href="/venue">Venue</a></li>
+        <li><a href="/merch">Merch</a></li>
+        <li><a href="/about">About Tfest</a></li>
+    </ul>
+
+    <a href="" class="md:hidden flex justify-end">
+        <span class="material-symbols-outlined">
+            menu
+        </span>
+    </a>
+</nav>
